cb83e68c050970257c45e88ddd2bbf76488e3c3a
Abermal <34352604+Abermal@users.noreply.github.com>
2020-05-28 19:01:30 +0300

    Update price_sniffer.py

--- price_sniffer.py
+++ price_sniffer.py
@@ -5,8 +5,8 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
-BASE_URL = 'https://www.kogan.com/nz/'
-TOKEN = '1099662762:AAF7N5qKUo9y_ALvThyakYacrXuIlTrjY5o'
+
+TOKEN = ''
 CHAT_ID = 256498289
 TELEGRAM_API_SEND_MSG = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
